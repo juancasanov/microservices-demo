8ecd73a79dbce3b85909a191edc0028dd65ce355
Juan Casanova <129115242+juancasanov@users.noreply.github.com>
2026-04-12 20:40:54 -0500

    feat: agregar Terraform para provisioning de AKS e integrar en pipeline de infraestructura

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "centralus"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the AKS cluster"
+  type        = number
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_D2s_v3"
+}
